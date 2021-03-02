5a3a8fd9f26652a74eb14cd55baec10b8b7c126d
Adrian Quej <Adrian Quej@LAPTOP-LCPEV7PP>
2021-03-01 22:18:04 -0600

    PRIMER COMMIT

---
+++ src/tiposdedatosarea59903/TiposDeDatosArea59903.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea59903;
+
+/**
+ *
+ * @author Adrian Quej
+ */
+public class TiposDeDatosArea59903 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Los siguientes valores represetan los lados de un rectangulo
+        
+        int lado1 = 56;
+        int lado2 = 79;
+        
+        // Obteniendo el area del rectangulo
+        
+        int area = (lado1 * lado2);
+        System.out.println("El lado 1 del rectangulo es =" + lado1);
+        System.out.println("El lado 2 del rectangulo es =" + lado2);
+        System.out.println("El area del rectangulo es =" + area);
+       
+    }
+    
+}
